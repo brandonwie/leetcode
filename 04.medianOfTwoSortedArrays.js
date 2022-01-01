7909a724f9497dd3ef6562ea5454b0e0bfb9e34d
Brandon Wie <brandonwie.cs@gmail.com>
2022-01-01 21:58:27 +0900

    add(medianOfTwoSortedArrays): need to fix the function without sort method

--- 04.medianOfTwoSortedArrays.js
+++ 04.medianOfTwoSortedArrays.js
@@ -24,27 +24,56 @@ const findMedianWithSortFunction = (nums1, nums2) => {
     ? (merge[merge.length / 2 - 1] + merge[merge.length / 2]) / 2
     : merge[Math.floor(merge.length / 2)];
 };
-console.log(findMedianWithSortFunction([1, 3], [2, 4]));
 
 // using a custom sort method
 function findMedianSortedArrays(nums1, nums2) {
   // binary search algorithm
-  let A = nums1;
-  let B = nums2;
-  // make A smaller array
-  if (A.length > B.length) {
-    A = B;
-    B = A;
-  }
+  if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
   let total = nums1.length + nums2.length;
   // calculate half value of merged array (e.g. 13 => half === 6)
   let half = Math.floor((nums1.length + nums2.length) / 2);
-  // set pointers in the smaller array
-  let l = 0;
-  let r = A.length - 1;
 
-  while (l <= r) {
-    let leftPartition = Math.floor((l + r) / 2); // A
-    let rightPartition = half - leftPartition - 2; // B
+  //* 1. set pointers for the smaller array to find the median of it
+  // by doing so, no need to calculate the median of the larger array
+  // just need to calculate half - smaller array's half (this will be )
+  let leftPointer = 0;
+  let rightPointer = nums1.length - 1; // end pointer starts at the index as the same as the last index of the smaller array
+
+  while (leftPointer <= rightPointer) {
+    // left partition for the smaller array (*mid index)
+    let smallLeftPartition = Math.floor((leftPointer + rightPointer) / 2);
+    console.log('smaller left partition:', smallLeftPartition);
+    // right partition for the larger array (what's left for the larger array)
+    let largeLeftPartition = half - smallLeftPartition - 2;
+    console.log('larger left partition:', largeLeftPartition);
+    //! think finding the median of merged array as partitioning with having left and right the same size (if it's even)
+    // compare both left partition's far right with corresponding right partition's far left
+    let maxLeftSmall =
+      smallLeftPartition < 0 ? -Infinity : nums1[smallLeftPartition];
+    let maxLeftLarge =
+      largeLeftPartition < 0 ? -Infinity : nums2[largeLeftPartition];
+    let minRightSmall =
+      smallLeftPartition >= nums1.length
+        ? Infinity
+        : nums1[smallLeftPartition + 1];
+    let minRightLarge =
+      largeLeftPartition >= nums2.length
+        ? Infinity
+        : nums2[largeLeftPartition + 1];
+    // the easiest case: both far right values of left partitions are smaller then corresponding right partitions' minimum
+    if (maxLeftSmall <= minRightLarge && maxLeftLarge <= minRightSmall) {
+      // compare which one is larger between two max values
+      const maxLeftPartition = Math.max(maxLeftSmall, maxLeftLarge);
+      if (total % 2 === 1) return maxLeftPartition;
+      return (maxLeftPartition + Math.min(minRightSmall, minRightLarge)) / 2;
+      // larger left partition's max value is BIGGER than the smaller right partition's min value
+      //! move left pointer to mid + 1
+    } else if (maxLeftSmall <= minRightLarge)
+      leftPointer = smallLeftPartition + 1;
+    // opposite to the above case
+    //! move left pointer to mid -1
+    else rightPointer = smallLeftPartition - 1;
   }
 }
+
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
--- README.md
+++ README.md
@@ -2,12 +2,33 @@
 
 ## Disclaimer
 
-- Not sure if I can keep "one LeetCode problem per day", but I will do my best.
+- Not sure if I can keep "three LeetCode problems per week", but I will do my best.
 - This repo will not only contain my solutions, but also solutions that meet my preferences, or those are recorded as the fastest on LeetCode.
 - Hence, not all solutions are mine, and I will leave comments if it's not solely mine.
 - `README.md` wil be written in 'lastest to earliest' order
 
-### #4. Median of Two Sorted arrays (12/30/2021)
+### #4. [HARD] Median of Two Sorted arrays (12/30/2021)
+
+(update 01/01/2022)<br>
+
+1. we want the size of the left partition as roughly the half of the size of the merged array
+2. the most intuitive way of doing this is by taking half of each array
+3. once you find the middle index of the smaller array by,
+
+   ```javascript
+   let smallerArrayMidIndex = Math.floor((startIndex + endIndex) / 2);
+   ```
+
+4. You can repeat the same process for the larger array, but you don't need to calculate the amount that you need to take from bigger array because,
+
+   ```javascript
+   // this will do the job
+   let largerArrayMidIndex = half - smallerArrayMidIndex;
+   ```
+
+5. Compare one array to another because each array is sorted; therefore, no need to order anything within each array (compare )
+
+6.
 
 The fastest solution is almost the same as my solution, but I don't know why it runs way slower than the one.<br>
 `Array.sort((a,b)=>a-b)` was kind of a key to this solution because `sort()` doesn't apply to negative numbers by itself.<br>
@@ -25,3 +46,7 @@ Using less functions will help to decrease the time it takes.
 ### #1 Two Sum
 
 the fastest solutions and the popular ones are using Objects(HashMap style in Java) and one for-loop
+
+```
+
+```
